28d0e5b1f9760dbb02511235fd3248aa01611a54
qvarkk <xumdes@gmail.com>
2023-02-09 18:45:47 +0500

    Set href of anchors to #

--- php/header.php
+++ php/header.php
@@ -3,19 +3,19 @@
     <img class="header-logo" src="./img/logo.png">
   </div>
   <nav class="header-center">
-    <a class="header-home" href="">Home</a>
-    <a class="header-order" href="">Order</a>
-    <a class="header-locations" href="">Locations</a>
-    <a class="header-news" href="">News</a>
+    <a class="header-home" href="#">Home</a>
+    <a class="header-order" href="#">Order</a>
+    <a class="header-locations" href="#">Locations</a>
+    <a class="header-news" href="#">News</a>
   </nav>
 
    <div class="header-right-side">
      <div class="header-profile">
        <img class="header-pfp" id="headerMenuBtn" src="./img/default-pfp.jpg" alt="Profile picture">
        <div class="header-pf-menu inactive" id="headerMenu">
-         <a class="header-pf-profile" href="">Profile</a>
-        <a class="header-pf-settings" href="">Settings</a>
-         <a class="header-pf-logout" href="">Log Out</a>
+         <a class="header-pf-profile" href="#">Profile</a>
+        <a class="header-pf-settings" href="#">Settings</a>
+         <a class="header-pf-logout" href="#">Log Out</a>
        </div>
      </div>
      <a class="header-cart" href="">
