94c5bc31fa81bf409ed7ae088b5d2be7deeed8a8
qvarkk <xumdes@gmail.com>
2023-02-09 16:19:03 +0500

    Changed pallet and fixed Header

--- index.php
+++ index.php
@@ -11,31 +11,29 @@
 </head>
 <body>
   <header class="header">
-    <div class="header-container">
-      <div class="header-left-side">
-        <img class="header-logo" src="./img/logo.png" alt="">
-      </div>
-      <div class="header-center">
-        <a class="header-home" href="">Home</a>
-        <a class="header-order" href="">Order</a>
-        <a class="header-locations" href="">Locations</a>
-        <a class="header-news" href="">News</a>
-      </div>
+     <div class="header-left-side">
+       <img class="header-logo" src="./img/logo.png" alt="">
+     </div>
+     <div class="header-center">
+       <a class="header-home" href="">Home</a>
+       <a class="header-order" href="">Order</a>
+       <a class="header-locations" href="">Locations</a>
+       <a class="header-news" href="">News</a>
+     </div>
 
-      <div class="header-right-side">
-        <div class="header-profile">
-          <img class="header-pfp" id="headerMenuBtn" src="./img/default-pfp.jpg" alt="Profile picture">
-          <div class="header-pf-menu inactive" id="headerMenu">
-            <a class="header-pf-profile" href="">Profile</a>
-            <a class="header-pf-settings" href="">Settings</a>
-            <a class="header-pf-logout" href="">Log Out</a>
-          </div>
-        </div>
-        <a class="header-cart" href="">
-          <img src="./img/icons/shopping-cart.svg" alt="Cart">
-        </a>
-      </div>
-    </div>
+     <div class="header-right-side">
+       <div class="header-profile">
+         <img class="header-pfp" id="headerMenuBtn" src="./img/default-pfp.jpg" alt="Profile picture">
+         <div class="header-pf-menu inactive" id="headerMenu">
+           <a class="header-pf-profile" href="">Profile</a>
+          <a class="header-pf-settings" href="">Settings</a>
+           <a class="header-pf-logout" href="">Log Out</a>
+         </div>
+       </div>
+       <a class="header-cart" href="">
+         <img class="header-cart" src="./img/icons/shopping-cart.svg" alt="Cart">
+       </a>
+     </div>
   </header>
   
   <main class="main"></main>
