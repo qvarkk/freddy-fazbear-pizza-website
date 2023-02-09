03481bede6fee6d5658cd6f5c7bac34860e18e67
qvarkk <xumdes@gmail.com>
2023-02-09 22:01:58 +0500

    Changed hrefs

--- php/header.php
+++ php/header.php
@@ -3,7 +3,7 @@
     <img class="header-logo" src="./img/logo.png">
   </div>
   <nav class="header-center">
-    <a class="header-home" href="#">Home</a>
+    <a class="header-home" href="./index.php">Home</a>
     <a class="header-order" href="#">Order</a>
     <a class="header-locations" href="#">Locations</a>
     <a class="header-news" href="#">News</a>
@@ -13,7 +13,7 @@
      <div class="header-profile">
        <img class="header-pfp" id="headerMenuBtn" src="./img/default-pfp.jpg" alt="Profile picture">
        <div class="header-pf-menu inactive" id="headerMenu">
-         <a class="header-pf-profile" href="#">Profile</a>
+         <a class="header-pf-profile" href="./register.php">Profile</a>
         <a class="header-pf-settings" href="#">Settings</a>
          <a class="header-pf-logout" href="#">Log Out</a>
        </div>
