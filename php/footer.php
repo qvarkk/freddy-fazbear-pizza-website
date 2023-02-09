7244c56c8a011073edad5af2b816ed2a3116cf0e
qvarkk <xumdes@gmail.com>
2023-02-09 22:01:48 +0500

    Changer hrefs

--- php/footer.php
+++ php/footer.php
@@ -2,10 +2,10 @@
   <div class="footer-container">
     <img class="footer-logo" src="./img/logo.png">
     <nav class="footer-nav">
-      <a class="footer-home" href="">Home</a>
-      <a class="footer-order" href="">Order</a>
-      <a class="footer-locations" href="">Locations</a>
-      <a class="footer-news" href="">News</a>
+      <a class="footer-home" href="./index.php">Home</a>
+      <a class="footer-order" href="#">Order</a>
+      <a class="footer-locations" href="#">Locations</a>
+      <a class="footer-news" href="#">News</a>
     </nav>
     <p class="footer-copy">&copy; Freddy Fazbear's Pizza | 1944-2023</p>
   </div>
